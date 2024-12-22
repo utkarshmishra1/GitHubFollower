267c484622a0c71fade90f68d7a479c3ca0149be
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-12-22 21:21:42 +0530

    made GFButtons(custom button) and GFTextField(custom text field)

---
+++ GitHubFollower/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  GitHubFollower
+//
+//  Created by utkarsh mishra on 22/12/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
