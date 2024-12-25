50da1a38862292bb76b49ff270f3eb5d3de2f41b
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-12-25 19:46:00 +0530

    made follower list view controlller, passes data between views, configure navigation bar colour(in scene delegate)

---
+++ GitHubFollower/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVCViewController.swift
+//  GitHubFollower
+//
+//  Created by utkarsh mishra on 25/12/24.
+//
+
+import UIKit
+
+class FollowerListVCViewController: UIViewController {
+    var username: String
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+}
