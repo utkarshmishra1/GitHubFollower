58a8dc5efb34ae3226b2c33c8dd0941b16eaef73
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-12-25 22:18:41 +0530

    fixes some bugs

---
+++ GitHubFollower/GFAlertVC.swift
@@ -0,0 +1,29 @@
+//
+//  GFAlertVC.swift
+//  GitHubFollower
+//
+//  Created by utkarsh mishra on 25/12/24.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
