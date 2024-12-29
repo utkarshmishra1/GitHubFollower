9e8bfe61adfa65ebdd05aee2868d269a1409800b
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-12-29 14:30:43 +0530

    Pagination for next 100 follower

--- GitHubFollower/Controller/FollowerListVC.swift
+++ GitHubFollower/Controller/FollowerListVC.swift
@@ -15,6 +15,8 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
+    var page = 1
+    var hasMoreFollowers: Bool = true
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section,Follower>! // it should be hashable
     
@@ -22,9 +24,9 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         configureViewController()
         configureCollectionView()
-        getFollowers()
+        getFollowers(username: username, page: page)
         configureDataSource()
-        }
+    }
     
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,33 +43,35 @@ class FollowerListVC: UIViewController {
     func configureCollectionView(){
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
-      
+        
     }
     
     
-//     network call
-//    Use guard when you need to ensure conditions are met before proceeding and want to exit early if they're not.
-//    Use if for simple checks, especially when no unwrapping or binding is involved.
-    func getFollowers(){
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+    //     network call
+    //    Use guard when you need to ensure conditions are met before proceeding and want to exit early if they're not.
+    //    Use if for simple checks, especially when no unwrapping or binding is involved.
+    func getFollowers(username: String, page: Int){
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return } // using this we don't need to add ? to the self
             
             switch result{
             case .success(let followers):
-                self.followers = followers // here network call has strong reference to our followerListVC- so we do self weak
+                if followers.count < 100 { self.hasMoreFollowers = false }
+                self.followers.append(contentsOf: followers) // here network call has strong reference to our followerListVC- so we do self weak
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something Went Wrong", message: error.rawValue, buttonTile: "OK")
-            
-                }
+                
             }
+        }
     }
     
-//     so basically ye function abhi ka snap lega aur baad ka lega aur dono ko animatedly dikhaayega
-//    iska use: Collection view data ko easy aur smoothly update karne ke liye hota hai.
+    //     so basically ye function abhi ka snap lega aur baad ka lega aur dono ko animatedly dikhaayega
+    //    iska use: Collection view data ko easy aur smoothly update karne ke liye hota hai.
     func configureDataSource() {
         dataSource = UICollectionViewDiffableDataSource<Section, Follower>( collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
             // Dequeue reusable cell
@@ -79,7 +83,7 @@ class FollowerListVC: UIViewController {
             return cell
         })
     }
-
+    
     func updateData() {
         // Create a new snapshot
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
@@ -92,8 +96,26 @@ class FollowerListVC: UIViewController {
             self.dataSource.apply(snapshot, animatingDifferences: true)
         }
     }
+    
+}
+    
+    extension FollowerListVC: UICollectionViewDelegate{
+        func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+            let offsetY             = scrollView.contentOffset.y
+            let contentHeight       = scrollView.contentSize.height
+            let frameHeight         = scrollView.frame.size.height
+            
+            if offsetY > contentHeight - frameHeight {
+                guard hasMoreFollowers else { return }
+                page += 1
+                getFollowers(username: username, page: page)
+            }
+
+        }
+    }
+    
 
      
-    }
+    
    
 
