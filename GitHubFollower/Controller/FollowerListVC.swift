3ae90aeb5ae533491585e13c60a848bcb00dfe32
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-12-29 16:37:01 +0530

    Empty State View

--- GitHubFollower/Controller/FollowerListVC.swift
+++ GitHubFollower/Controller/FollowerListVC.swift
@@ -66,6 +66,13 @@ class FollowerListVC: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers) // here network call has strong reference to our followerListVC- so we do self weak
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go ahead and follow them!"
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                    
+                }
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something Went Wrong", message: error.rawValue, buttonTile: "OK")
