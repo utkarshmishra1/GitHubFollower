de8c47b19fa786ac07c9c1e18dc5926510f412c4
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2025-01-01 16:20:04 +0530

    made favorite view

--- GitHubFollower/Controller/FollowerListVC.swift
+++ GitHubFollower/Controller/FollowerListVC.swift
@@ -44,6 +44,8 @@ class FollowerListVC: UIViewController {
     func configureViewController(){
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
         
     }
     
@@ -124,7 +126,40 @@ class FollowerListVC: UIViewController {
         }
     }
     
-}
+    @objc func addButtonTapped() {
+            showLoadingView()
+            
+            NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+                guard let self = self else { return }
+                self.dismissLoadingView()
+                
+                switch result {
+                case .success(let user):
+                    self.addUserToFavorites(user: user)
+                    
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                }
+            }
+        }
+    
+    func addUserToFavorites(user: User) {
+           let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+           
+           PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+               guard let self = self else { return }
+               
+               guard let error = error else {
+                   self.presentGFAlertOnMainThread(title: "Success!", message: "You have successfully favorited this user 🚀", buttonTitle: "Hooray!")
+                   return
+               }
+               
+               self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
+           }
+       }
+   }
+    
+    
     
 extension FollowerListVC: UICollectionViewDelegate{
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
--- GitHubFollower/Utilities/GFError.swift
+++ GitHubFollower/Utilities/GFError.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 enum GFError: String, Error {
-    case invalidUsername     = "Invalid URL"
-    case unableToComplete    = "Unable to complete your request. Please check your internet connection."
-    case invalidResponse     = "Invalid response from the server. Please try again."
-    case invalidData         = "Invalid data received from the server. Please try again."
+    case invalidUsername        = "Invalid URL"
+    case unableToComplete       = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse        = "Invalid response from the server. Please try again."
+    case invalidData            = "Invalid data received from the server. Please try again."
+    case unableToFavorite       = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites     = "You've already favorited this user, you must really like them."
 }
