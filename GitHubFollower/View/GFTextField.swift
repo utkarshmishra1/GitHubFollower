1dbf23e56fc49f33f7cf0312ae4a083f7a21abfc
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2025-01-02 18:48:16 +0530

    refractor

--- GitHubFollower/View/GFTextField.swift
+++ GitHubFollower/View/GFTextField.swift
@@ -13,7 +13,6 @@ class GFTextField: UITextField {
         super.init(frame: frame)
         configure()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
